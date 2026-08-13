82522c9b7a8659b0dac58533b5d38553055507c1
ustark108-collab <ustark108@gmail.com>
2026-08-13 12:00:00 +0530

    Added Day 4 solutions

---
+++ day4/Day4Q2.c
@@ -0,0 +1,20 @@
+/*
+Q8 (User Inputs, Operations & Output)
+Write a program to find and display the sum of the first n natural numbers.*/
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}
