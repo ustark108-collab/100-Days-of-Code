b501821844d63cac3706d32f15b3148c6746857f
ustark108-collab <ustark108@gmail.com>
2026-08-15 12:00:00 +0530

    Added Day 6 solutions

---
+++ day6/Day6Q1.c
@@ -0,0 +1,19 @@
+/*Q11 (Conditional Statements)
+Write a program to input an integer and check whether it is even or odd using if–else.*/
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is even \n", num);
+    } else {
+        printf("%d is odd \n", num);
+    }
+
+    return 0;
+}
---
+++ day6/Day6Q2.c
@@ -0,0 +1,25 @@
+/*
+Q12 (Conditional Statements)
+Write a program to input an integer and check whether it is positive, negative or zero using nested if-else.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	int num;
+
+	printf("Enter an integer: ");
+	scanf("%d", &num);
+
+	if (num >= 0) {
+		if (num == 0) {
+			printf("The number is zero\n");
+		} else {
+			printf("%d is positive\n", num);
+		}
+	} else {
+		printf("%d is negative\n", num);
+	}
+
+	return 0;
+}
