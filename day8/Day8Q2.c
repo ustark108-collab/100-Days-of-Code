8e9a023baff0c40ee3e9c2683ab4be53dd1b1a46
ustark108-collab <ustark108@gmail.com>
2026-08-17 12:00:00 +0530

    Added Day 8 solutions

---
+++ day8/Day8Q2.c
@@ -0,0 +1,28 @@
+/*
+Q16 (Conditional Statements)
+Write a program to input three numbers and find the largest among them using if–else.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c)
+    {
+        printf("Largest number = %d", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("Largest number = %d", b);
+    }
+    else
+    {
+        printf("Largest number = %d", c);
+    }
+
+    return 0;
+}
