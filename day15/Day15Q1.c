c58500e860ca72f20a8f528dee8dae9f440731b9
ustark108-collab <ustark108@gmail.com>
2026-08-24 12:00:00 +0530

    Added Day 15 solutions

---
+++ day15/Day15Q1.c
@@ -0,0 +1,25 @@
+/*
+Q29 (Loops without Arrays/Strings)
+Write a program to calculate the factorial of a number.*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.");
+    } else {
+        for (i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+
+        printf("Factorial of %d = %lld", n, factorial);
+    }
+
+    return 0;
+}
---
+++ day15/Day15Q2.c
@@ -0,0 +1,22 @@
+/*
+Q30 (Loops without Arrays/Strings)
+Write a program to reverse a given number.*/
+
+#include <stdio.h>
+
+int main() {
+    int num, reverse = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+        reverse = reverse * 10 + remainder;
+        num = num / 10;
+    }
+
+    printf("Reversed number = %d", reverse);
+
+    return 0;
+}
