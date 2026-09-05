c6821e0b12be6c71b07ea9388e80122f199d57f6
ustark108-collab <ustark108@gmail.com>
2026-09-05 12:00:00 +0530

    Added Day 27 solutions

---
+++ day27/Day27Q2.c
@@ -0,0 +1,21 @@
+Q54: Write a program to print the following pattern:
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include <stdio.h>
+int main(){ for(int i=1;i<=4;i++){ for(int j=1;j<=4-i;j++) printf(" "); for(int j=1;j<=2*i-1;j++) printf("*"); printf("\n"); } for(int i=3;i>=1;i--){ for(int j=1;j<=4-i;j++) printf(" "); for(int j=1;j<=2*i-1;j++) printf("*"); printf("\n"); } return 0; }
